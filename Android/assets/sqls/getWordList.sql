49cfc63ba9ca55fea60e35ccc7fd25acbac0520e
blackhjd <blackhjd@gmail.com>
2014-03-13 19:00:24 +0800

    modify the sql based on 3 new columns in word table

--- Android/assets/sqls/getWordList.sql
+++ Android/assets/sqls/getWordList.sql
@@ -1,14 +1,6 @@
-select b.word_vocabulary, b.BE_phonetic_symbol, b.BE_sound, b.is_listening, b.is_speaking, b.is_reading, b.is_writing, b.part_of_speech, b.explanation, p.tinyPic
-from (
-	select w.word_vocabulary, w.BE_phonetic_symbol, w.BE_sound, w.is_listening, w.is_speaking, w.is_reading, w.is_writing, e.part_of_speech, e.content explanation
-	from words w, explanations e
-	where w.word_vocabulary = e.word_vocabulary
-	and w.word_vocabulary like ?
-	and e.category='basic' 
-	and e.seq = 1)  b
-left outer join pics p
-on b.word_vocabulary = p.word_vocabulary
-and p.isPrimary = 1
+select b.word_vocabulary, b.BE_phonetic_symbol, b.BE_sound, b.is_listening, b.is_speaking, b.is_reading, b.is_writing, b.part_of_speech, b.explanation, b.tinyPic
+from words b
+where  b.word_vocabulary like ?
 order by %s %s
 limit ?
 offset ?
