8a9b4050c7245309865b928de1d12f0b75f7c70c
Black <blackhjd@gmail.com>
2014-03-21 21:04:18 +0800

    delete gen

--- Android/gen/tt/lab/android/ieltspass/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package tt.lab.android.ieltspass;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
